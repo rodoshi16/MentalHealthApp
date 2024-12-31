7c9cbce5e2475227268e0811781bb40a7696cf86
mondalr3 <rodoshi.mondal@mail.utoronto.ca>
2024-12-31 17:59:06 -0500

    Build the UI for the application

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { ChatBox } from './Components/Box'; 
+
+function App() {
+  return (
+    <div className="App">
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Box.js
@@ -0,0 +1,24 @@
+import react from 'react'; 
+
+export const ChatBox = () => {
+    return (
+        <div className= "Bot-container">
+            <div className='Bot-header'>
+                <div className='Bot-logo'>
+                <img 
+                    src= {"https://i.ibb.co/fSNP7Rz/icons8-chatgpt-512.png"} 
+                    class= "Bot-logo" 
+                    width= "70" 
+                    height= "70"
+                />
+                </div>
+                <div className= "Bot-title">
+                    <h2>MindMate</h2>
+                    <p2>I am here to listen!</p2>
+                </div>
+            </div>
+        </div>
+
+    )
+
+}
