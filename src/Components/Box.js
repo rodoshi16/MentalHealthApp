1e0b1420f95981f8dfabede07449f3bc75d6b408
mondalr3 <rodoshi.mondal@mail.utoronto.ca>
2025-01-04 20:20:43 -0500

    Added textbar and send icon

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -1,6 +1,72 @@
-import react from 'react'; 
+import react, { useState } from 'react'; 
+import axios from 'axios';
+import sendImage from './send-4008.png'
+
+
 
 export const ChatBox = () => {
+
+    const [userInput, setUserInput] = useState('');
+    const [chatHistory, setChatHistory] = useState([]); 
+    const [botResponse, setbotResponse] = useState(''); 
+
+    const handleSendMessage = () => {
+        if (userInput.trim() == ""){
+            return;  
+        }
+
+        const newMessage = {sender: "User", message: userInput}
+        setChatHistory(prevHistory => [...prevHistory, newMessage])
+        setUserInput(""); 
+
+        setUserInput(""); 
+
+
+
+    }
+
+
+    // useEffect(() => {
+    //     fetch("/mindmate").then((res) => {
+    //         setdata({
+                
+    //         })
+    //     }
+    // )
+    // })
+
+    // axios.get('/api/users')
+    //     .then(response => {
+    //         console.log(response.data)
+    //     })
+    //     .catch(error => {
+    //         console.error('Error fetching data: ', error)
+    //     })
+
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault(); 
+
+    //     fetch(
+    //         'http://localhost:5000/mindmate', 
+    //         {
+    //             method: 'POST', 
+    //             body: JSON.stringify({
+    //                 todo, 
+    //                 completed: false
+    //             })
+    //         .then(res => res.json())
+    //         .then(() => {
+    //             console.log("Mindmate is running")
+    //         })
+    //         .catch(e => {
+    //             console.log(e.message)
+    //         })
+
+    //         }
+    //     )
+    // }
+
     return (
         <div className= "Bot-container">
             <div className='Bot-header'>
@@ -16,6 +82,20 @@ export const ChatBox = () => {
                     <h2>MindMate</h2>
                     <p2>I am here to listen!</p2>
                 </div>
+            </div>
+            <input 
+            type= "text"
+            className= "TextBar"
+            placeholder="Type your message..."
+            autocomplete="on"
+            value={userInput}
+            onChange= {(e) => setUserInput(e.target.value)}
+            ></input>
+            <div className= "SendIcon" onClick= {handleSendMessage}>
+                <img
+                    src={sendImage}
+                />
+
             </div>
         </div>
 
